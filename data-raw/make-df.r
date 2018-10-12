809a2904e8dd7d85af65548e3e6d143087a3c30c
Tianqi Zhao <tianqi.zhao@yale.edu>
2018-10-12 15:59:09 -0400

    testtravis

--- data-raw/make-df.r
+++ data-raw/make-df.r
@@ -1,3 +1,9 @@
-lm_patho <- read.csv("df.csv")
-dir.create("../data")
-save(lm_patho, file = "../data/lm_patho.rda")
+#lm_patho <- read.csv("df.csv")
+ridge_train <- read.csv("ridge_train.csv")
+ridge_test <- read.csv("ridge_test.csv")
+#dir.create("../data")
+save(ridge_train, file = "../data/ridge_train.rda")
+save(ridge_test, file = "../data/ridge_test.rda")
+
+save(ridge_train, file = "../vignettes/ridge_train.rda")
+save(ridge_test, file = "../vignettes/ridge_test.rda")
